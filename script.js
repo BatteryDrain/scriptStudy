3e35f48a1208bf71b4797ab9445ecdd43f60e53d
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-06-25 10:06:12 -0600

    fiddling

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const textarea = document.getElementById('script');
+const dump = document.getElementById('dump');
 
 textarea.addEventListener('input', () => {
-    textarea.style.height = 'auto'; // Reset height
-    textarea.style.height = textarea.scrollHeight + 'px'; // Set new height
+    textarea.style.height = 'auto';
+    textarea.style.height = textarea.scrollHeight + 'px';
     console.log("got it!");
 })
 
-
+function dump(){
+    
+}
