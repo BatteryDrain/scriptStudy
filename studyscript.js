24c1fa98cc1b4e32b50d0b414b41a4b0a8d2c619
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-07-17 15:45:34 -0600

    fiddling

--- studyscript.js
+++ studyscript.js
@@ -3,11 +3,19 @@ console.log(SCRIPT);
 LINES = SCRIPT.split("\n");
 VERSIONS = [];
 VERSIONS.push(SCRIPT);
+list();
+
 
 para = document.createElement('textarea');
 para.id = "out";
     para.textContent = SCRIPT;
-    console.log(SCRIPT);
     para.disabled = true;
     para.rows= LINES.length;
 dump.appendChild(para);
+
+
+function list(){
+    for(i = 0; i < SCRIPT.length; i++){
+        
+    }
+}
