19401b66217c99ddaed92789858d8ca03196a03b
BatteryDrain <115777815+BatteryDrain@users.noreply.github.com>
2025-07-30 16:14:49 -0600

    made list of words to hide and added incrementing buttons

--- studyscript.js
+++ studyscript.js
@@ -1,21 +1,76 @@
 SCRIPT = window.localStorage.getItem("script");
 console.log(SCRIPT);
 LINES = SCRIPT.split("\n");
+VERSION = [];
 VERSIONS = [[]];
-VERSIONS.push(SCRIPT);
+VERSIONS[0].push(SCRIPT);
+VERSIONNUM = 0;
+CWords = [
+    "and",
+    "or",
+]
+PRIORITIES = [];
 list();
 
 
+
 para = document.createElement('textarea');
-para.id = "out";
+    para.id = "out";
     para.textContent = SCRIPT;
     para.disabled = true;
     para.rows= LINES.length;
 dump.appendChild(para);
 
-
+  
 function list(){
-    for(i = 0; i < SCRIPT.length; i++){
-        
+    VERSION.push("");
+    temp = SCRIPT.split(" ");
+    for(i = 0; i < temp.length; i++){
+        PRIORITIES.push("");
+        VERSION.push(temp[i]);
+        if(temp[i] != CWords[0] && temp[i] != CWords[1] && temp[i] != ""){
+            num = randomInt(1, temp.length);
+            while(PRIORITIES.includes(num)){
+                num = randomInt(1, temp.length );
+            }
+            PRIORITIES[i] = num;
+        }
+        else{
+            PRIORITIES[i] = "";           
+        }
     }
+    function randomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    VERSION[0] = SCRIPT;
+    VERSIONS.push(VERSION);
+}
+
+up.addEventListener('click', () => {
+    if(VERSIONNUM < VERSIONS[1].length - 1){
+        VERSIONNUM++;
+    }
+    setVersionNumber(VERSIONNUM);
+    setTimeout();
+});
+
+down.addEventListener('click', () => {
+    if(VERSIONNUM > 0){
+        VERSIONNUM--;
+    }
+    setVersionNumber(VERSIONNUM);
+    setTimeout();
+});
+
+function setVersionNumber(number){
+    if(number == 0){
+        ver.innerHTML = "original";
+    }
+    else{
+        ver.innerHTML = "version "+VERSIONNUM;
+    }
+}
+
+function setOut(){
+    
 }
